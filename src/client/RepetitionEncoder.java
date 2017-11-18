f994493e463ddbadbcc80506690d5c422318c647
FrogTravel <e.levchenko@innopolis.ru>
2017-11-18 13:47:25 +0300

    Comments added

--- src/client/RepetitionEncoder.java
+++ src/client/RepetitionEncoder.java
@@ -9,6 +9,12 @@ import java.util.PriorityQueue;
  */
 public class RepetitionEncoder {
 
+    /**
+     * Takes the initial message and repeats every byte desired number of times
+     * Writes result to result array and returns it
+     * @param message to encode
+     * @param rep number of times to repeat each byte
+     */
     public static byte[] encode(byte[] message, int rep) {
         byte[] result = new byte[message.length * rep];
         int counter = 0;
@@ -23,11 +29,17 @@ public class RepetitionEncoder {
     }
 
 
+    /**
+     * For every entity in array there is a map<byte, Integer> that holds how many times each byte is in the message,
+     * Finds element with maximum frequency and wrote it to the result array
+     * @param message to decode
+     * @param rep how many times each symbol was repeated
+     * @return initial message
+     */
     public static byte[] decode(byte[] message, int rep) {
         byte[] result = new byte[message.length / rep];
         int messageCounter = 0;
         for (int i = 0; i < message.length / rep; i++) {
-            int priority;
             Map<Byte, Integer> frequency = new HashMap<>();//byte, how many times
             for (int j = 0; j < rep; j++) {
                 int repetitions = 0;
